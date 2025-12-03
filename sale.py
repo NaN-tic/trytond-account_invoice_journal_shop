d7f49a14d5fc64889f75e4ce08845b2bff670dcb
Raimon Esteve <raimonesteve@gmail.com>
2025-12-03 15:18:05 +0100

    Rename _get_invoice_sale in _get_invoice on sale

--- sale.py
+++ sale.py
@@ -3,7 +3,7 @@ from trytond.pool import PoolMeta
 class Sale(metaclass=PoolMeta):
     __name__ = 'sale.sale'
 
-    def _get_invoice_sale(self):
-        invoice = super()._get_invoice_sale()
+    def _get_invoice(self):
+        invoice = super()._get_invoice()
         invoice.on_change_type()
         return invoice
